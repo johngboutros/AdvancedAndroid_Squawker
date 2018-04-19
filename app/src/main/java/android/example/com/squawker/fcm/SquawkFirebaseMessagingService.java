c1e887d711b21537e6207997409d3394a1ff4828
John Boutros <john@zealous.co>
2018-04-19 21:02:55 +0200

    JB: format

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
@@ -27,22 +27,22 @@ public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
     private static final int NEW_SQUAWK_NOTIFICATION_ID = 234;
 
     // DONE (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
-    // DONE (2) As part of the new Service - Override onMessageReceived. This method will
-    // be triggered whenever a squawk is received. You can get the data from the squawk
-    // message using getData(). When you send a test message, this data will include the
-    // following key/value pairs:
-    // test: true
-    // author: Ex. "TestAccount"
-    // authorKey: Ex. "key_test"
-    // message: Ex. "Hello world"
-    // date: Ex. 1484358455343
-    // TODO (3) As part of the new Service - If there is message data, get the data using
-    // the keys and do two things with it :
-    // TODO 1. Display a notification with the first 30 character of the message
-    // TODO 2. Use the content provider to insert a new message into the local database
-    // Hint: You shouldn't be doing content provider operations on the main thread.
-    // If you don't know how to make notifications or interact with a content provider
-    // look at the notes in the classroom for help.
+        // DONE (2) As part of the new Service - Override onMessageReceived. This method will
+        // be triggered whenever a squawk is received. You can get the data from the squawk
+        // message using getData(). When you send a test message, this data will include the
+        // following key/value pairs:
+            // test: true
+            // author: Ex. "TestAccount"
+            // authorKey: Ex. "key_test"
+            // message: Ex. "Hello world"
+            // date: Ex. 1484358455343
+        // TODO (3) As part of the new Service - If there is message data, get the data using
+        // the keys and do two things with it :
+            // TODO 1. Display a notification with the first 30 character of the message
+            // TODO 2. Use the content provider to insert a new message into the local database
+            // Hint: You shouldn't be doing content provider operations on the main thread.
+            // If you don't know how to make notifications or interact with a content provider
+            // look at the notes in the classroom for help.
 
 
     @Override
